fdb2af0505f0f83b247080ebd7c61b734e359542
Michael Heroux <maherou@sandia.gov>
2013-09-26 03:22:39 +0000

    Adding missing PHP file
    git-svn-id: svn+ssh://software.sandia.gov/svn/hpcg/trunk@169 5e06a57e-6b39-4354-92a0-249a7d0c5410

---
+++ web/HPCG_BOF.php
@@ -0,0 +1,15 @@
+<?php
+  include 'common/page.php';
+  $page = new Page();
+  $page->setPathToWebRoot('');
+  $page->setPageTitle('SC13 BOF');
+  $page->setNavIdentifier('events');
+?>
+
+<?php include 'common/header.html' ?>
+
+<div class="breadcrumb"><a href="events.php">Events</a> - SC13 Birds-of-a-Feather</div>
+
+<p>More announced later </p>
+
+<?php include 'common/footer.html' ?>

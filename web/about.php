dd9820c32f56d6f0837ac7e76e7dc301bbb83fa1
Michael Heroux <maherou@sandia.gov>
2013-09-26 13:47:01 +0000

    Made changes to address Horst Simon's comments.
    M    web/about.php
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/hpcg/trunk@172 5e06a57e-6b39-4354-92a0-249a7d0c5410

--- web/about.php
+++ web/about.php
@@ -12,8 +12,13 @@
 
 <h3>About the HPCG Benchmark Project</h3>
 
-<p>The HPCG Benchmark project is an effort to create a more relevant metric for ranking HPC systems 
-than the current Top 500 High Performance LINPACK code.</p>
+<p>The HPCG Benchmark project is an effort to create a more relevant metric for ranking HPC systems than the 
+High Performance LINPACK (HPL) benchmark, that is currently used by the TOP500 benchmark.  The computational and data
+access patterns of HPL are no longer driving computer system designs in directions that are beneficial
+to many important scalable applications.  HPCG is designed to exercise computational and data access patterns that more
+closely match a broad set of important applications, and to give incentive to computer system designers to invest in 
+capabilities that will have impact on the collective performance of these applications.
+</p>
 
     <p><b>HPCG is a complete, stand-alone code that measures the performance of basic operations in a unified code:</b>
       

7e2e43c8b396fd4724b357e03c05f5f3e213c499
Michael Heroux <maherou@sandia.gov>
2013-12-04 23:08:53 +0000

    Added the first two FAQs.
    M    web/faq.php
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/hpcg/trunk@204 5e06a57e-6b39-4354-92a0-249a7d0c5410

--- web/faq.php
+++ web/faq.php
@@ -13,7 +13,29 @@
     <ul>
 	<li><b>FAQ:</b></li>
         <ul>
-         <li><b>Will High Performance Linpack be replaced?</b>
+         <li><b>Will HPCG replace High Performance Linpack (HPL)?</b>: 
+			We do not intend to eliminate HPL.  HPCG will provide 
+			an alternative ranking of the TOP 500 machines. We
+			expect that HPCG will take several years to both mature
+			and emerge as a widely-visible metrics.
+
+         <li><b>Isn't HPCG just another version of the STREAMS benchmark?</b>: 
+			If the reference version of HPCG is used for performance
+			analysis, the fraction of time spent in the (unoptimized) 
+			sparse kernels (in particular ComputeSPMV and ComputeSYMGS)
+			will be very high, and HPCG performance will be dominated by
+			memory system performance.  In this case, the benchmark will
+			give rankings that are very similar to STREAMS for computer
+			systems that have a good reduction network, or have few nodes.
+			<p>
+			However, this is true for many benchmarks.  If HPL were executed
+			with reference Fortran BLAS kernels, HPL would also look like a
+			STREAMS benchmark.
+			<p>
+			Warnings have been added to HPCG reports to let the benchmarker
+			know that performance will be suboptimal when using reference
+			kernels.
+			
         </ul>
     </ul>
 

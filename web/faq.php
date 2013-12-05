df1e1ebc4482291c0e184dcc48adf52f1f14ab4b
Michael Heroux <maherou@sandia.gov>
2013-12-05 11:04:45 +0000

    Wordsmithing of STREAM comparison.
    M    web/faq.php
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/hpcg/trunk@206 5e06a57e-6b39-4354-92a0-249a7d0c5410

--- web/faq.php
+++ web/faq.php
@@ -10,33 +10,30 @@
 
 <div class="breadcrumb">FAQ</div>
 
-    <ul>
-	<li><b>FAQ:</b></li>
-        <ul>
-         <li><b>Will HPCG replace High Performance Linpack (HPL)?</b>: 
-			We do not intend to eliminate HPL.  HPCG will provide 
-			an alternative ranking of the TOP 500 machines. We
-			expect that HPCG will take several years to both mature
-			and emerge as a widely-visible metric.
+<ul>
+<li><b>Will HPCG replace High Performance Linpack (HPL)?</b>: 
+	We do not intend to eliminate HPL.  HPCG will provide 
+	an alternative ranking of the TOP 500 machines. We
+	expect that HPCG will take several years to both mature
+	and emerge as a widely-visible metric.
 
-         <li><b>Isn't HPCG just another version of the STREAMS benchmark?</b>: 
-			If the reference version of HPCG is used for performance
-			analysis, the fraction of time spent in the (unoptimized) 
-			sparse kernels (in particular ComputeSPMV and ComputeSYMGS)
-			will be very high, and HPCG performance will be dominated by
-			memory system performance.  In this case, the benchmark will
-			give rankings that are very similar to STREAMS for computer
-			systems that have a good reduction network, or have few nodes.
-			<p>
-			However, this is true for many benchmarks.  If HPL were executed
-			with reference Fortran BLAS kernels, HPL would also look like a
-			STREAMS benchmark.
-			<p>
-			Warnings have been added to HPCG reports to let the benchmarker
-			know that performance will be suboptimal when using reference
-			kernels.
-			
-        </ul>
-    </ul>
+<li><b>Isn't HPCG just another version of the STREAM benchmark?</b>: 
+	If the reference version of HPCG is used for performance
+	analysis, the fraction of time spent in the (unoptimized) 
+	sparse kernels (in particular ComputeSPMV and ComputeSYMGS)
+	will be very high, and HPCG performance will be dominated by
+	memory system performance.  In this case, for computer systems
+	with a good reduction networks, or HPCG runs using few MPI processes,
+	the benchmark will
+	give rankings that are very similar to STREAM.
+	<p>
+	However, this is true for many benchmarks.  If HPL were executed
+	with reference Fortran computational kernels, HPL would also look like a
+	STREAM benchmark.
+	<p>
+	Warnings have been added to HPCG reports to let the benchmarker
+	know that performance will be suboptimal when using reference
+	kernels.
+</ul>
 
 <?php include 'common/footer.html' ?>

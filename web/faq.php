16f8be5559d0c605ede8d4ee6943bfe542772033
Piotr Luszczek <luszczek@eecs.utk.edu>
2014-01-28 14:01:37 +0000

    Small adjustment of white space for the existing two questions.
    git-svn-id: svn+ssh://software.sandia.gov/svn/hpcg/trunk@221 5e06a57e-6b39-4354-92a0-249a7d0c5410

--- web/faq.php
+++ web/faq.php
@@ -11,15 +11,15 @@
 <div class="breadcrumb">FAQ</div>
 
 <ul>
-<li><b>Will HPCG replace High Performance Linpack (HPL)?</b>: 
-	We do not intend to eliminate HPL.  HPCG will provide 
-	an alternative ranking of the TOP 500 machines. We
+<li><b>Will HPCG replace High Performance Linpack (HPL)?</b>:
+	We do not intend to eliminate HPL.  HPCG will provide
+	an alternative ranking of the TOP500 machines. We
 	expect that HPCG will take several years to both mature
 	and emerge as a widely-visible metric.
 
-<li><b>Isn't HPCG just another version of the STREAM benchmark?</b>: 
+<li><b>Isn't HPCG just another version of the STREAM benchmark?</b>:
 	If the reference version of HPCG is used for performance
-	analysis, the fraction of time spent in the (unoptimized) 
+	analysis, the fraction of time spent in the (unoptimized)
 	sparse kernels (in particular ComputeSPMV and ComputeSYMGS)
 	will be very high, and HPCG performance will be dominated by
 	memory system performance.  In this case, for computer systems
@@ -28,8 +28,8 @@
 	give rankings that are very similar to STREAM.
 	<p>
 	However, this is true for many benchmarks.  If HPL were executed
-	with reference Fortran computational kernels, HPL would also look like a
-	STREAM benchmark.
+        with reference Fortran computational kernels (Basic Linear Algebra
+        Subprograms), HPL would also look like a STREAM benchmark.
 	<p>
 	Warnings have been added to HPCG reports to let the benchmarker
 	know that performance will be suboptimal when using reference

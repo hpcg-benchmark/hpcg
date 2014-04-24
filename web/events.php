fdd115dac2ff715ab6fac86be74708a5c60f9f7b
Michael Heroux <maherou@sandia.gov>
2014-04-24 16:51:28 +0000

    Updating events page to include ASCR Workshop, ISC talk.
    git-svn-id: svn+ssh://software.sandia.gov/svn/hpcg/trunk@242 5e06a57e-6b39-4354-92a0-249a7d0c5410

--- web/events.php
+++ web/events.php
@@ -11,7 +11,16 @@
 <div class="breadcrumb">Events</div>
 
 
-<p>HPCG-related events.</p>
+<p>Past HPCG-related events.</p>
+<ul>
+<li><a href="http://www.netlib.org/utk/people/JackDongarra/SLIDES/sc13-top500-hpcg-FINAL.pdf"> HPCG Overview Talk from SC'13 TOP 500 BOF.(.pdf)</a></li>
+<li> <a href="https://www.orau.gov/hpcg2014/default.htm">DOE ASCR HPCG Workshop</a>, March 25, 2014, Washington, DC, USA.</li>
+</ul>
+
+<p>Upcoming HPCG-related events.</p>
+<ul>
+<li> <a href="http://www.isc-events.com/isc14_ap/presentationdetails.htm?t=presentation&o=22&a=select&ra=sessiondetails"> HPCG Presentation at ISC'14</a> ISC'14, June 26, 2014 , Leipzig, Germany.</li></ul>
+
 
 
 <?php include 'common/footer.html' ?>

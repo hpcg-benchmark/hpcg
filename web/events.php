655268cc4450908d75a7bcc977acc9b9b395ffd7
Michael Heroux <maherou@sandia.gov>
2014-06-26 09:38:22 +0000

    Added link to ISC'14 talk
    git-svn-id: svn+ssh://software.sandia.gov/svn/hpcg/trunk@252 5e06a57e-6b39-4354-92a0-249a7d0c5410

--- web/events.php
+++ web/events.php
@@ -15,12 +15,13 @@
 <ul>
 <li><a href="http://www.netlib.org/utk/people/JackDongarra/SLIDES/sc13-top500-hpcg-FINAL.pdf"> HPCG Overview Talk from SC'13 TOP 500 BOF.(.pdf)</a></li>
 <li> <a href="https://www.orau.gov/hpcg2014/default.htm">DOE ASCR HPCG Workshop</a>, March 25, 2014, Washington, DC, USA.</li>
+<li> <a href="http://www.isc-events.com/isc14_ap/presentationdetails.htm?t=presentation&o=22&a=select&ra=sessiondetails"> HPCG Presentation at ISC'14</a> ISC'14, June 26, 2014 , Leipzig, Germany.</li>
 </ul>
 
 <p>Upcoming HPCG-related events.</p>
 <ul>
-<li> <a href="http://www.isc-events.com/isc14_ap/presentationdetails.htm?t=presentation&o=22&a=select&ra=sessiondetails"> HPCG Presentation at ISC'14</a> ISC'14, June 26, 2014 , Leipzig, Germany.</li></ul>
-
+<li> <a href="http://supercomputing.org"> HPCG BOF at SC'14 (Proposed)</a> SC'14, November, 2014 , New Orleans, LA.</li>
+</ul>
 
 
 <?php include 'common/footer.html' ?>
--- web/publications.php
+++ web/publications.php
@@ -14,6 +14,7 @@
 <li><a href="doc/HPCG-Benchmark.pdf"> Toward a New Metric for Ranking High Performance Computing Systems (.pdf)</a></li>
 <li><a href="doc/HPCG-Specification.pdf"> HPCG Technical Specification (.pdf)</a></li>
 <li><a href="http://www.netlib.org/utk/people/JackDongarra/SLIDES/sc13-top500-hpcg-FINAL.pdf"> HPCG Overview Talk from SC'13 TOP 500 BOF.(.pdf)</a></li>
+<li><a href="http://www.netlib.org/utk/people/JackDongarra/SLIDES/hpcg-isc-0614.pdf"> HPCG: One Year Later, from ISC'14.(.pdf)</a></li>
 </ul>
 
 <?php include 'common/footer.html' ?>

cae65086de0f4c0efe9d3e883cb92d413668539c
Piotr Luszczek <luszczek@icl.utk.edu>
2016-12-20 17:05:35 -0500

    Change to more concise naming of output files

--- src/init.cpp
+++ src/init.cpp
@@ -149,17 +149,15 @@ HPCG_Init(int * argc_p, char ** *argv_p, HPCG_Params & params) {
 
   time ( &rawtime );
   ptm = localtime(&rawtime);
-  sprintf( fname, "hpcg_log_%04d.%02d.%02d.%02d.%02d.%02d.txt",
+  sprintf( fname, "hpcg%04d%02d%02dT%02d%02d%02d.txt",
       1900 + ptm->tm_year, ptm->tm_mon+1, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec );
 
   if (0 == params.comm_rank) {
     HPCG_fout.open(fname);
   } else {
 #if defined(HPCG_DEBUG) || defined(HPCG_DETAILED_DEBUG)
-    char local[15];
-    sprintf( local, "%d_", params.comm_rank );
-    sprintf( fname, "hpcg_log_%s%04.d%02d.%02d.%02d.%02d.%02d.txt", local,
-        1900 + ptm->tm_year, ptm->tm_mon+1, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec );
+    sprintf( fname, "hpcg%04d%02d%02dT%02d%02d%02d_%d.txt",
+        1900 + ptm->tm_year, ptm->tm_mon+1, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec, params.comm_rank );
     HPCG_fout.open(fname);
 #else
     HPCG_fout.open(NULLDEVICE);

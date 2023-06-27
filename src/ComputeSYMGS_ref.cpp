e9e6c3a463efaea9a38f8a2aedabd715d4f50cd1
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2023-06-27 21:32:17 +0100

    Add old symgs routine and reduce multigrid levels from 4 to 1

--- src/ComputeSYMGS_ref.cpp
+++ src/ComputeSYMGS_ref.cpp
@@ -66,70 +66,110 @@ int ComputeSYMGS_ref( const SparseMatrix & A, const Vector & r, Vector & x) {
   ExchangeHalo(A,x);
 #endif
 
+  const local_int_t nrow = A.localNumberOfRows;
+  double ** matrixDiagonal = A.matrixDiagonal;  // An array of pointers to the diagonal entries A.matrixValues
   const double * const rv = r.values;
   double * const xv = x.values;
 
-  global_int_t nx = A.geom->nx;
-  global_int_t ny = A.geom->ny;
-  global_int_t nz = A.geom->nz;
-  global_int_t ng = 1; // number of ghost points FIXME should be declared "more globally"
-  const local_int_t nrow_ghost = (nx+2*ng)*(ny+2*ng)*(nz+2*ng); // local number of rows *including* ghost points
-  double* xg = new double[nrow_ghost]{0.0}; // Copy of x with ghost points
+  for (local_int_t i=0; i< nrow; i++) {
+    const double * const currentValues = A.matrixValues[i];
+    const local_int_t * const currentColIndices = A.mtxIndL[i];
+    const int currentNumberOfNonzeros = A.nonzerosInRow[i];
+    const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
+    double sum = rv[i]; // RHS value
 
-  // copy xv to xg TODO HACK fix this
-  for (local_int_t ix=0; ix<nx; ++ix) {
-    for (local_int_t iy=0; iy<ny; ++iy) {
-      for (local_int_t iz=0; iz<nz; ++iz) {
-        xg[idx(ix,iy,iz,nx,ny,nz,ng)] = xv[idx(ix,iy,iz,nx,ny,nz)];
-      }
+    for (int j=0; j< currentNumberOfNonzeros; j++) {
+      local_int_t curCol = currentColIndices[j];
+      sum -= currentValues[j] * xv[curCol];
     }
+    sum += xv[i]*currentDiagonal; // Remove diagonal contribution from previous loop
+
+    xv[i] = sum/currentDiagonal;
+
   }
 
-  const double currentDiagonal = 26.0; // Current diagonal value CHANGEME when changing stencil
+  // Now the back sweep.
 
-#ifndef HPCG_NO_OPENMP
-  #pragma omp parallel for
-#endif
-  for (local_int_t ix=0; ix<nx; ++ix) {
-    for (local_int_t iy=0; iy<ny; ++iy) {
-      for (local_int_t iz=0; iz<nz; ++iz) {
-        double sum = rv[idx(ix,iy,iz,nx,ny,nz)]; // RHS value
-        for(int sx=-1; sx<=1; ++sx) {
-          for(int sy=-1; sy<=1; ++sy) {
-            for(int sz=-1; sz<=1; ++sz) {
-              sum -= -1.*xg[idx(ix+sx, iy+sy, iz+sz, nx, ny, nz, ng)];
-            }
-          }
-        }
-        sum += -1.*xg[idx(ix+0, iy+0, iz+0, nx, ny, nz, ng)]; // Remove diagnoal component entirely
+  for (local_int_t i=nrow-1; i>=0; i--) {
+    const double * const currentValues = A.matrixValues[i];
+    const local_int_t * const currentColIndices = A.mtxIndL[i];
+    const int currentNumberOfNonzeros = A.nonzerosInRow[i];
+    const double  currentDiagonal = matrixDiagonal[i][0]; // Current diagonal value
+    double sum = rv[i]; // RHS value
 
-        xg[idx(ix,iy,iz,nx,ny,nz,ng)] = sum/currentDiagonal;
-      }
+    for (int j = 0; j< currentNumberOfNonzeros; j++) {
+      local_int_t curCol = currentColIndices[j];
+      sum -= currentValues[j]*xv[curCol];
     }
+    sum += xv[i]*currentDiagonal; // Remove diagonal contribution from previous loop
+
+    xv[i] = sum/currentDiagonal;
   }
 
-#ifndef HPCG_NO_OPENMP
-  #pragma omp parallel for
-#endif
-  for (local_int_t ix=nx-1; ix>=0; --ix) {
-    for (local_int_t iy=ny-1; iy>=0; --iy) {
-      for (local_int_t iz=nz-1; iz>=0; --iz) {
-        double sum = rv[idx(ix,iy,iz,nx,ny,nz)]; // RHS value
-        for(int sx=-1; sx<=1; ++sx) {
-          for(int sy=-1; sy<=1; ++sy) {
-            for(int sz=-1; sz<=1; ++sz) {
-              sum -= -1.*xg[idx(ix+sx, iy+sy, iz+sz, nx, ny, nz, ng)];
-            }
-          }
-        }
-        sum += -1.*xg[idx(ix+0, iy+0, iz+0, nx, ny, nz, ng)]; // Remove diagnoal component entirely
+  //const double * const rv = r.values;
+  //double * const xv = x.values;
 
-        xv[idx(ix,iy,iz,nx,ny,nz)] = sum/currentDiagonal;
-      }
-    }
-  }
+  //global_int_t nx = A.geom->nx;
+  //global_int_t ny = A.geom->ny;
+  //global_int_t nz = A.geom->nz;
+  //global_int_t ng = 1; // number of ghost points FIXME should be declared "more globally"
+  //const local_int_t nrow_ghost = (nx+2*ng)*(ny+2*ng)*(nz+2*ng); // local number of rows *including* ghost points
+  //double* xg = new double[nrow_ghost]{0.0}; // Copy of x with ghost points
 
-  delete[] xg;
+  //// copy xv to xg TODO HACK fix this
+  //for (local_int_t ix=0; ix<nx; ++ix) {
+    //for (local_int_t iy=0; iy<ny; ++iy) {
+      //for (local_int_t iz=0; iz<nz; ++iz) {
+        //xg[idx(ix,iy,iz,nx,ny,nz,ng)] = xv[idx(ix,iy,iz,nx,ny,nz)];
+      //}
+    //}
+  //}
+
+  //const double currentDiagonal = 26.0; // Current diagonal value CHANGEME when changing stencil
+
+//#ifndef HPCG_NO_OPENMP
+  //#pragma omp parallel for
+//#endif
+  //for (local_int_t ix=0; ix<nx; ++ix) {
+    //for (local_int_t iy=0; iy<ny; ++iy) {
+      //for (local_int_t iz=0; iz<nz; ++iz) {
+        //double sum = rv[idx(ix,iy,iz,nx,ny,nz)]; // RHS value
+        //for(int sx=-1; sx<=1; ++sx) {
+          //for(int sy=-1; sy<=1; ++sy) {
+            //for(int sz=-1; sz<=1; ++sz) {
+              //sum -= -1.*xg[idx(ix+sx, iy+sy, iz+sz, nx, ny, nz, ng)];
+            //}
+          //}
+        //}
+        //sum += -1.*xg[idx(ix+0, iy+0, iz+0, nx, ny, nz, ng)]; // Remove diagnoal component entirely
+
+        //xg[idx(ix,iy,iz,nx,ny,nz,ng)] = sum/currentDiagonal;
+      //}
+    //}
+  //}
+
+//#ifndef HPCG_NO_OPENMP
+  //#pragma omp parallel for
+//#endif
+  //for (local_int_t ix=nx-1; ix>=0; --ix) {
+    //for (local_int_t iy=ny-1; iy>=0; --iy) {
+      //for (local_int_t iz=nz-1; iz>=0; --iz) {
+        //double sum = rv[idx(ix,iy,iz,nx,ny,nz)]; // RHS value
+        //for(int sx=-1; sx<=1; ++sx) {
+          //for(int sy=-1; sy<=1; ++sy) {
+            //for(int sz=-1; sz<=1; ++sz) {
+              //sum -= -1.*xg[idx(ix+sx, iy+sy, iz+sz, nx, ny, nz, ng)];
+            //}
+          //}
+        //}
+        //sum += -1.*xg[idx(ix+0, iy+0, iz+0, nx, ny, nz, ng)]; // Remove diagnoal component entirely
+
+        //xv[idx(ix,iy,iz,nx,ny,nz)] = sum/currentDiagonal;
+      //}
+    //}
+  //}
+
+  //delete[] xg;
 
   return 0;
 }

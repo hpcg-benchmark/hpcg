4c98006607c66795fd6fbe52a9a0c1bb98d657a0
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2023-06-28 12:12:45 +0100

    Swap 3d index direction so ix moves fastest instead of iz. This matches original version.

--- src/idx.hpp
+++ src/idx.hpp
@@ -3,8 +3,7 @@
 
 inline int idx(const int ix, const int iy, const int iz, const int nx, const int ny, const int nz, const int ng=0) {
   // input index of (0, 0, 0) corresponds to first *interior* point. Ghost points are accessed with (-1, -1, -1).
-  //return (iz+ng)*(nx+2*ng)*(ny+2*ng)+(iy+ng)*(nx+2*ng)+(ix+ng);
-  return (ix+ng)*(nz+2*ng)*(ny+2*ng)+(iy+ng)*(nz+2*ng)+(iz+ng);
+  return (iz+ng)*(nx+2*ng)*(ny+2*ng) + (iy+ng)*(nx+2*ng) + (ix+ng);
 }
 
 #endif  // IDX_HPP
